d13be6ecc334bbe96d6ec3c6ce074a98abafeea2
Mitali Manjarekar <manjarekar.m@husky.neu.edu>
2020-01-20 11:41:50 -0500

    added project files

---
+++ src/Business/Organization/DoctorOrganization.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+/**
+ *
+ * @author manja
+ */
+public class DoctorOrganization extends Organization {
+
+    public DoctorOrganization() {
+        
+    }
+    
+}
---
+++ src/Business/Organization/Organization.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import Business.Employee.EmployeeDirectory;
+import java.util.ArrayList;
+import Business.WorkQueue.*;
+
+/**
+ *
+ * @author manja
+ */
+public class Organization {
+    private String name;
+    private WorkQueue workQueue;
+    private EmployeeDirectory employeeDirectory;
+    private UserAccountDirectory userAccountDirectory;
+    private int organizationID;
+    private static int counter=0;
+    
+    public enum Type{
+        Admin("Admin Organization"), Ambulance("Ambulance Organization"), Doctor("Doctor Organization"), Lab("Lab Organization"), Rehabilitaion_Center("Rehabilitation Center Organization");
+        private String value;
+        private Type(String value) {
+            this.value = value;
+        }
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public Organization(String name) {
+        this.name = name;
+        workQueue = new WorkQueue();
+        employeeDirectory = new EmployeeDirectory();
+        userAccountDirectory = new UserAccountDirectory();
+        organizationID = counter;
+        ++counter;
+    }
+
+ //   public abstract ArrayList<Role> getSupportedRole();
+    
+    public UserAccountDirectory getUserAccountDirectory() {
+        return userAccountDirectory;
+    }
+
+    public int getOrganizationID() {
+        return organizationID;
+    }
+
+    public EmployeeDirectory getEmployeeDirectory() {
+        return employeeDirectory;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWorkQueue(WorkQueue workQueue) {
+        this.workQueue = workQueue;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
